b5719880aa4888ca7347e92b96386b59af8a2d8c
Me <me@example.com>
2017-12-05 12:34:16 +0200

    app finished

--- src/SelfDestructTimerComponent/SelfDestructTimerComponent.js
+++ src/SelfDestructTimerComponent/SelfDestructTimerComponent.js
@@ -1,24 +1,26 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 
 class SelfDestructTimerComponent extends React.Component{
     constructor(props, context){
         super(props, context);
-        this.state = {count:42};
+        this.state = {count:3};
     }
-    changeDigitInCounter (){
-        if (this.state.count>0){
-            document
-            .getElementsByClassName("counter")
-            .innerHTML=this.setState({count: this.setState.count-1});
+    changeDigitInCounter = () => {
+        this.setState({count: this.state.count-1});
+    
+    if (this.state.count===0){
+        document.body.style.backgroundColor = "red";
+        clearInterval(this.intervalID);
         }
+    } 
+
+    componentDidMount = ()=>{
+        this.intervalID = setInterval(this.changeDigitInCounter, 1000);
+
     }
-    componentWillMount(){
-        var intervalID = setInterval(changeDigitInCounter, 1000);
-    }
-    render(){
+    render = () =>{
         return (
-            <div className='counter'>{}</div>
+            <div>{this.state.count}</div>
         )
     }
 }
